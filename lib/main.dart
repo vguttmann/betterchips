21b154becf9b8f8968ad9e9170e492d8a0b500ee
fischerliste <fischerliste@gmail.com>
2022-09-11 20:46:11 +0200

    set up firebase & firebase hosting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
+
 
 void main() {
   runApp(const MyApp());
