a4363280231ddc583bd36e05acb46c628f4064e7
fischerliste <fischerliste@gmail.com>
2022-09-15 20:59:00 +0200

    some minor stuff and new routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'main_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -8,18 +7,71 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const App());
+  runApp(
+    MaterialApp(
+      title: 'Named Routes Demo',
+      // Start the app with the "/" named route. In this case, the app starts
+      // on the FirstScreen widget.
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => const LoginScreen(),
+        // When navigating to the "/login" route, build the FirstScreen widget.
+        '/login': (context) => const LoginScreen(),
+        // When navigating to the "/game" route, build the SecondScreen widget.
+        '/game': (context) => const GameScreen(),
+      },
+    ),
+  );
 }
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'betterchips-7d23c',
-      theme: ThemeData(primaryColor: const Color(0xFF3D814A)),
-      home: ToDoList(),
+    return Scaffold(
+      appBar: AppBar(
+        /// disables the "back" button in the AppBar
+        //automaticallyImplyLeading: false,
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          // Within the `FirstScreen` widget
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pushReplacementNamed(context, '/');
+          },
+          child: const Text('Launch screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        /// disables the "back" button in the AppBar
+        //automaticallyImplyLeading: false,
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          // Within the SecondScreen widget
+          onPressed: () {
+            // Navigate back to the first screen by popping the current route
+            // off the stack.
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
     );
   }
 }
