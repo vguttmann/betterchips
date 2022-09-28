c10f6b56dbb358f35b8a55e744c9f876332bf434
fischerliste <fischerliste@gmail.com>
2022-09-28 14:39:29 +0200

    created game.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:betterchips/game.dart';
 import 'package:betterchips/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -30,22 +32,4 @@ Future<void> main() async {
       },
     ),
   );
-}
-
-class GameScreen extends StatelessWidget {
-  const GameScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        /// disables the "back" button in the AppBar
-        //automaticallyImplyLeading: false,
-        title: const Text('Second Screen'),
-      ),
-      body: const Center(
-        child: Text('Game'),
-      ),
-    );
-  }
 }
